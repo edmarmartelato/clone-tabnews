17d050d9a4173bbfb6058074053a6be216df7a6f
edmarmartelato <52167876+edmarmartelato@users.noreply.github.com>
2026-04-03 20:04:05 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
   //response.status(200).send("alunos do curso.dev são pessoas acima da média");
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   response.status(200).json({ chave: "são acima da média" });
 }
 
