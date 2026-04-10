beebe8728899ac8bbffe3643d15e38c11a499465
edmarmartelato <52167876+edmarmartelato@users.noreply.github.com>
2026-04-10 16:58:23 +0000

    banco de dados vazando

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,38 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  //response.status(200).send("alunos do curso.dev são pessoas acima da média");
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "são acima da média" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionvalue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValues =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseOpenedConnectionsResult = await database.query(
+    "SELECT * FROM pg_stat_activity WHERE datname = 'local_db' ;",
+  );
+
+  console.log(databaseOpenedConnectionsResult);
+
+  const databaseOpenedConnectionsValues =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  console.log(" aqui : " + databaseOpenedConnectionsValues);
+
+  response.status(200).json({
+    updated_At: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionvalue,
+        max_connections: parseInt(databaseMaxConnectionsValues),
+        opened_connections: databaseOpenedConnectionsValues,
+      },
+    },
+  });
 }
 
 export default status;
